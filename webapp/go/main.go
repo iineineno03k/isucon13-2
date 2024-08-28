7fde23627e5d42891eccff54ce7e5524b9027f46
ryusei-sanada <ryusei_sanada@r.recruit.co.jp>
2024-08-28 18:28:47 +0900

    InterpolateParamsをtrueに設定

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -8,8 +8,10 @@ import (
 	"log"
 	"net"
 	"net/http"
+	_ "net/http/pprof"
 	"os"
 	"os/exec"
+	"runtime"
 	"strconv"
 
 	"github.com/go-sql-driver/mysql"
@@ -65,6 +67,7 @@ func connectDB(logger echo.Logger) (*sqlx.DB, error) {
 	conf.Passwd = "isucon"
 	conf.DBName = "isupipe"
 	conf.ParseTime = true
+	conf.InterpolateParams = true
 
 	if v, ok := os.LookupEnv(networkTypeEnvKey); ok {
 		conf.Net = v
@@ -119,6 +122,12 @@ func initializeHandler(c echo.Context) error {
 }
 
 func main() {
+	//main関数の最初に記述する。
+	runtime.SetBlockProfileRate(1)
+	runtime.SetMutexProfileFraction(1)
+	go func() {
+		log.Fatal(http.ListenAndServe("localhost:6060", nil))
+	}()
 	e := echo.New()
 	e.Debug = true
 	e.Logger.SetLevel(echolog.DEBUG)
